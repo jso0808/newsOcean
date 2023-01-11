e09203dd085babb914f64976ea1168f83a383244
marudaga <counterpencil@naver.com>
2023-01-11 11:58:06 +0900

    2023-01-11 박하람 subsection,section

--- newsOcean/src/main/java/com/sp/app/main/News.java
+++ newsOcean/src/main/java/com/sp/app/main/News.java
@@ -1,30 +1,43 @@
 package com.sp.app.main;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+//컬렉션(테이블명)
+@Document(collection = "newsdata")
 public class News {
 	
 	private long memberNo;
-	
 	private long newsNo;
 	private String title;
 	private String originLink;
+	
 	private int hitCount;
 	private long categoryNo;
 	private int bookMarked;
 	private long bookMarkNum;
 
-	
 	// 몽고DB
-	// 위 변수에 합칠지 고민
-	private long crawlNo; // id컬럼일지 고민
+	@Id
+	private ObjectId crawlNo; // id컬럼일지 고민
 	private String crawlUrl;
 	private String crawlPress; // 언론사
+	@DateTimeFormat(iso = ISO.DATE_TIME)
 	private String crawlDate;
 	private String crawlTitle;
 	private String crawlContent;
-	private String crawlReport; // 기자 연락처
-	private String bCategory; // 대분류
-	private String sCategory; // 소분류
+	private String crawlSummary;
+	private String category; // 대분류
 	
+	public String getCrawlSummary() {
+		return crawlSummary;
+	}
+	public void setCrawlSummary(String crawlSummary) {
+		this.crawlSummary = crawlSummary;
+	}
 	public long getNewsNo() {
 		return newsNo;
 	}
@@ -55,10 +68,11 @@ public class News {
 	public void setCategoryNo(long categoryNo) {
 		this.categoryNo = categoryNo;
 	}
-	public long getCrawlNo() {
+	
+	public ObjectId getCrawlNo() {
 		return crawlNo;
 	}
-	public void setCrawlNo(long crawlNo) {
+	public void setCrawlNo(ObjectId crawlNo) {
 		this.crawlNo = crawlNo;
 	}
 	public String getCrawlUrl() {
@@ -91,23 +105,13 @@ public class News {
 	public void setCrawlContent(String crawlContent) {
 		this.crawlContent = crawlContent;
 	}
-	public String getCrawlReport() {
-		return crawlReport;
+	
+	
+	public String getCategory() {
+		return category;
 	}
-	public void setCrawlReport(String crawlReport) {
-		this.crawlReport = crawlReport;
-	}
-	public String getbCategory() {
-		return bCategory;
-	}
-	public void setbCategory(String bCategory) {
-		this.bCategory = bCategory;
-	}
-	public String getsCategory() {
-		return sCategory;
-	}
-	public void setsCategory(String sCategory) {
-		this.sCategory = sCategory;
+	public void setCategory(String category) {
+		this.category = category;
 	}
 	public int getBookMarked() {
 		return bookMarked;
--- newsOcean/src/main/java/com/sp/app/mypage/newsdata.java
+++
@@ -1,8 +0,0 @@
-package com.sp.app.mypage;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-//컬렉션(테이블명)
-@Document(collection = "newsdata")
-public class newsdata {
-}
